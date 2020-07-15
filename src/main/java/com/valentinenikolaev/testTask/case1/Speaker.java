8d65565547a5130d867b4d5076d3f8e021c222ff
Valentin Nikolaev <valentinenikolaev@gmail.com>
2020-07-15 11:17:34 +0300

    Change speaker class, remove runnable field

--- src/main/java/com/valentinenikolaev/testTask/case1/Speaker.java
+++ src/main/java/com/valentinenikolaev/testTask/case1/Speaker.java
@@ -2,11 +2,8 @@ package com.valentinenikolaev.testTask.case1;
 
 public class Speaker extends Thread {
 
-    private Runnable runnable;
-
     public Speaker(Runnable target) {
         super(target);
-        this.runnable = target;
     }
 
     @Override
